4f4ee72d968e783712f41d5813a4445fc8881c5a
darkshadowds08 <91031597+darkshadowds08@users.noreply.github.com>
2021-09-27 10:54:33 +0200

    Update default.js

--- src/config/default.js
+++ src/config/default.js
@@ -18,7 +18,7 @@ const config = {
   },
 
   refresh_token: REFRESH_TOKEN,
-  client_id: '6600e358-9328-4050-af82-0af9cdde796b',
+  client_id: '007ff7f2-5810-4aea-8808-7f24cc266fff',
   client_secret: CLIENT_SECRET,
 
   /**
